66b47c90ff753502f4824bd28a609473f710c226
Angel00Michel <106771574+Angel00Michel@users.noreply.github.com>
2023-05-11 09:40:07 -0700

    Part 1 Commit w/ Module work - ReadME. updated

---
+++ Module Challenge Folder/MechaCarChallenge.R
@@ -0,0 +1,24 @@
+# Use the library() function to load the dplyr package.
+library(dplyr)
+
+# PART 1
+
+# Import and read in the MechaCar_mpg.csv file as a dataframe.
+mechacar_df <- read.csv(file="MechaCar_mpg.csv")
+mechacar_df
+
+# Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), 
+# and add the dataframe you created in Step 4 as the data parameter.
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_df)
+
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_df)) 
+#R value = 0.7149
+#P-value = 5.35e-11
+
+# SAVE
+
+# PART 2
+
+
+
